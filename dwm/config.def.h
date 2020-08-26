5ab8acaa98a189f24f2a8e773ab193665cec98db
Nikola Radojevic <nikola@openbsd.radojevic.rs>
2020-08-26 14:54:29 +0200

    Bla Bla Bla Bla Bla

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -74,7 +74,7 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-const char term[] = "st";
+static const char term[] = "st";
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
