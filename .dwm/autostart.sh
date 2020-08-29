ad5e0741af607b807ca1c0588a69d2897b78ec8b
Nikola Radojevic <nikola@openbsd-malilaptop.radojevic.rs>
2020-08-29 11:51:53 +0200

    Podesavanje misa

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -6,3 +6,6 @@ slstatus &
 setxkbmap -layout rs,rs -variant latin, -option grp:win_space_toggle &
 xcompmgr &
 ~/.fehbg &
+doas wsconsctl mouse.reverse_scrolling=1
+doas wsconsctl mouse.tp.tapping=1
+
